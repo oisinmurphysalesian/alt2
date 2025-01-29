23b30b8b2c173ac6839d6555d864cca530710175
Ethan Broderick <ethan.broderick@salesiansecondarycollege.ie>
2025-01-29 10:18:05 +0000

    add function to csv file

--- src/seperate.py
+++ src/seperate.py
@@ -1,12 +1,15 @@
+
 #open file
+import pandas as pd
 
 def open_file():
     #adfs
+    print()
 
 ## determine segments
 
 
 ### write files
-def write_file():
+def write_file(dataframe, path):
     #asdf
-    
+    dataframe.to_csv(path)
